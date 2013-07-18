ba5791eac0528385bbf38d6502df66217444a04e
Lucas Machado <lukmail@gmail.com>
2013-07-18 00:06:36 -0300

    Upload de foto de atendente

--- SALMvc/Controllers/AtendenteController.cs
+++ SALMvc/Controllers/AtendenteController.cs
@@ -1,6 +1,7 @@
 ﻿using NHibernate;
 using SALClassLib.Masterdata.Model;
 using SALClassLib.Masterdata.Model.BO;
+using Utilitarios.BO;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,9 +12,8 @@ namespace SALMvc.Controllers
 {
     public class AtendenteController : Controller
     {
-        //
-        // GET: /Atendente/
         IList<Atendente> lista = new List<Atendente>();
+        String photoPath = "~/Uploads/Fotos/Funcionarios/Atendente/";
 
         public void Listar()
         {
@@ -42,19 +42,41 @@ namespace SALMvc.Controllers
         [HttpPost]
         public ActionResult Create(Atendente atendente)
         {
+
+
+            if (!ModelState.IsValid)
+            {
+                return View(atendente);
+            }
+
             AtendenteBO bo = new AtendenteBO();
+
             try
             {
-                bo.Incluir(atendente);
-                bo.Dispose();
+                ulong id = bo.Incluir(atendente);
+                if (!Request.Files["Foto"].FileName.Equals(""))
+                {
+                    HttpPostedFileBase postedFile = Request.Files["Foto"];
+                    atendente.Foto = Server.MapPath(photoPath) + String.Format("{0:0000000000}", id) + postedFile.FileName.Substring(postedFile.FileName.Length - 4, 4);
+                    postedFile.SaveAs(atendente.Foto);
+                    bo.Alterar(atendente);
+                }
                 TempData["flash"] = "Seu cadastro foi realisado com sucesso.";
+            }
+            catch (BOException ex)
+            {
+                ModelState.AddModelError("", ex.Message);
             }
             catch (Exception ex)
             {
-                TempData["flash"] = "Ocorreu um problema, tente novamente.";
-                TempData["ErrorMessage"] = ex.Message;
-                TempData["ErrorStackTrace"] = ex.StackTrace;
+                ModelState.AddModelError("", "Ocorreu um problema, tente novamente." + ex.Message);
             }
+            finally
+            {
+                if (bo != null)
+                    bo.Dispose();
+            }
+
             return RedirectToAction("Index");
         }
 
@@ -85,19 +107,44 @@ namespace SALMvc.Controllers
         [HttpPost]
         public ActionResult Edit(Atendente atendente)
         {
+            if (!ModelState.IsValid)
+            {
+                return View(atendente);
+            }
+
             AtendenteBO bo = new AtendenteBO();
             try
             {
+                HttpPostedFileBase postedFile = null;
+                if (!Request.Files["Foto"].FileName.Equals(""))
+                {
+                    if (System.IO.File.Exists(atendente.Foto)) System.IO.File.Delete(atendente.Foto);
+                    postedFile = Request.Files["Foto"];
+                    atendente.Foto = Server.MapPath(photoPath) + String.Format("{0:0000000000}", atendente.Id) + postedFile.FileName.Substring(postedFile.FileName.Length - 4, 4);
+                }
                 bo.Alterar(atendente);
+                if (postedFile != null)
+                {
+                    postedFile.SaveAs(atendente.Foto);
+                }
                 bo.Dispose();
-                TempData["flash"] = "Seu cadastro foi editado com sucesso.";
+                TempData["flash"] = "Seu cadastro foi realisado com sucesso.";
             }
-            catch
+            catch (BOException ex)
             {
-                TempData["flash"] = "Ocorreu um problema, tente novamente.";
+                ModelState.AddModelError("", ex.Message);
             }
-            Listar();
-            return View("Index", lista);
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "Ocorreu um problema, tente novamente.");
+            }
+            finally
+            {
+                if (bo != null)
+                    bo.Dispose();
+            }
+
+            return RedirectToAction("Index");
         }
 
         //
