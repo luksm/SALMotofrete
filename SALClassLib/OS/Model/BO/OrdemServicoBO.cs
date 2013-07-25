59cf849508aff1524d239781e716bef7c3ae3bcf
Lucas Machado <lukmail@gmail.com>
2013-07-24 21:43:55 -0300

    BO de Ordem de Serviço

---
+++ SALClassLib/OS/Model/BO/OrdemServicoBO.cs
@@ -0,0 +1,33 @@
+﻿using NHibernate;
+using SALClassLib.OS.Model.DAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utilitarios.BO;
+
+namespace SALClassLib.OS.Model.BO
+{
+    public class OrdemServicoBO : BO<OrdemServico>
+    {
+        private ISession sessao;
+
+        public OrdemServicoBO()
+        {
+            sessao = NHibernateHelper.GetCurrentSession();
+            OrdemServicoDAO dao = new OrdemServicoDAO(sessao);
+            base.Dao = dao;
+        }
+
+        ~OrdemServicoBO()
+        {
+            Dispose();
+        }
+
+        public new void Dispose()
+        {
+            NHibernateHelper.CloseSession(sessao);
+        }
+    }
+}
