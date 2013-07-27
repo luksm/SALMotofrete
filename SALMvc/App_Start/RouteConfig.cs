4f045a2e1f4540168e2449cfd3dded5faecc2166
Lucas Machado <lukmail@gmail.com>
2013-07-27 16:04:09 -0300

    Inicio do Front do Site

--- SALMvc/App_Start/RouteConfig.cs
+++ SALMvc/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace SALMvc
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "OrdemServico", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
